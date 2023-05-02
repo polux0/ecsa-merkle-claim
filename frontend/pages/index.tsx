8594473b572d368e71af705498ea6cea900d4e3d
polux0 <afidelo@protonmail.com>
2023-05-02 12:16:28 +0200

    fix: minor; moved `explanation` to variable

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -10,6 +10,7 @@ const tokenName: string = process.env.NEXT_PUBLIC_TOKEN_NAME ?? "Token Name";
 const heading: string = process.env.NEXT_PUBLIC_HEADING ?? "Some heading";
 const description: string =
   process.env.NEXT_PUBLIC_DESCRIPTION ?? "Some description";
+const explanation: string = `(1) Connect your wallet. (2) Make sure that you will have some ETH in your wallet to pay for the "gas" (the transaction fee), otherwise you won't be able to claim your tokens! (3) Sign the transaction with your the wallet. (4) If you need any help, please contact: whitelist@ecsa.io.`;
 
 export default function Home() {
   // Routing
@@ -76,7 +77,7 @@ export default function Home() {
           color: 'rgb(0, 0, 0)',
           background: 'rgb(255, 180, 165)',
           padding: '1.25rem'
-    }}>(1) Connect your wallet. (2) Make sure that you will have some ETH in your wallet to pay for the "gas" (the transaction fee), otherwise you won't be able to claim your tokens! (3) Sign the transaction with your the wallet. (4) If you need any help, please contact: whitelist@ecsa.io.</p>
+        }}>{explanation}</p>
 
         {/* Claim button */}
         {!address ? (
